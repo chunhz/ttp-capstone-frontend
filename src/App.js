e5cb76a88216ff8f8274468693eeaba0e9cf7e04
Bayardo Zelaya <bayardozelaya@gmail.com>
2020-06-17 16:34:38 -0400

    NavBar Style Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import MapComponent from "./components/MapComponent";
+import NavBar from "./styles/NavBar";
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <MapComponent />
       </div>
     );
---
+++ src/styles/NavBar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md" className="mb-5">
+        <NavbarBrand href="/">WiFi Finder</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/BayardoZelaya/ttp-capstone-frontend/tree/master">
+                Github
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
