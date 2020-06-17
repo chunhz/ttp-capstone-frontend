a4fbb23722980e167267592ae6175d42f638c5cd
Bayardo Zelaya <bayardozelaya@gmail.com>
2020-06-17 17:25:38 -0400

    added api call in axios for NYCHOTSPOT Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import MapComponent from "./components/MapComponent";
 import NavBar from "./styles/NavBar";
+import NYCHotSpotAPI from "./data/NYCHotSpotAPI";
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <NYCHotSpotAPI />
         <NavBar />
         <MapComponent />
       </div>
---
+++ src/data/NYCHotSpotAPI.js
@@ -0,0 +1,42 @@
+//use nyc public api endpoint at https://data.cityofnewyork.us/resource/yjub-udmw.json
+import React, { Component } from "react";
+import axios from "axios";
+
+export class NYCHotSpotAPI extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://data.cityofnewyork.us/resource/yjub-udmw.json", {
+        params: {
+          $limit: 50,
+        },
+      })
+      .then((response) => response.data)
+      .then((data) =>
+        this.setState({
+          data,
+        })
+      )
+      .catch((err) => console.log(err));
+    // fetch("https://data.cityofnewyork.us/resource/yjub-udmw.json")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     this.setState({ todos: data });
+    //     console.log(this.state.todos);
+    //   })
+    //   .catch(console.log);
+  }
+  render() {
+    console.log(this.state);
+    return <div>helloWord</div>;
+  }
+}
+
+export default NYCHotSpotAPI;
