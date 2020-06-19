133f6e089ca60a51656e08f3f1fcb009b8eda762
Chunhao Zheng <chz062009@gmail.com>
2020-06-18 21:43:42 -0400

    Change AddHotspotForm.js into .jsx component

--- src/components/AddHotspotForm.jsx
+++ src/components/AddHotspotForm.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import './form.css'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const hotspotForm = (props) => {
-  return (
-    <Form className='hotspot-form'>
+export default class hotspotForm extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <Form className='hotspot-form'>
        <h1 className='text-center'>Add New Hotspot</h1>
       <FormGroup>
         <Label >Hotspot Name</Label>
@@ -32,7 +41,9 @@ const hotspotForm = (props) => {
       </FormGroup>
       <Button>Submit</Button>
     </Form>
-  );
+      </div>
+    )
+  }  
 }   
 
 // SSID: TransitWirelessWiFi
@@ -40,5 +51,3 @@ const hotspotForm = (props) => {
 // Borough: Brooklyn
 // Wifi-Session: Free
 // Location-Type: Subway Station
-
-export default hotspotForm;
