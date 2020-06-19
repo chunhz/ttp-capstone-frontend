059b73027e2a8cdf0fd4c6bf8977bd9859349214
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-18 21:53:46 -0400

    save

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MapComponent from "./components/MapComponent";
 import NavBar from "./styles/NavBar";
 import AddHotspotForm from './components/AddHotspotForm'
+
 export class App extends Component {
   constructor(props){
     super(props);
