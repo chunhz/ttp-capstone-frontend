9b8282b14d03979bed522bacb5ad8aac91d0c9c8
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-19 10:15:21 -0400

    add onFormSubmit fumction

--- src/components/AddHotspotForm.jsx
+++ src/components/AddHotspotForm.jsx
@@ -9,42 +9,49 @@ export default class hotspotForm extends Component{
     this.state = {
 
     }
+
   }
+  onFormSubmit = (e)=>{
+    e.preventDefault();
+    //if (this.state.isValidName) this.props.addCampus(this.state);
+    //route component
+    
+  };
 
   render(){
     return(
       <div>
-        <Form className='hotspot-form'>
-       <h1 className='text-center'>Add New Hotspot</h1>
-      <FormGroup>
-        <Label >Hotspot Name</Label>
-        <Input type="text" name="text" id="hotspotName"/>
-      </FormGroup>
-      <FormGroup>
-        <Label >Address</Label>
-        <Input type="text" name="text" id="location" />
-      </FormGroup>
-      <FormGroup>
-        <Label >City</Label>
-        <Input type="text" name="text" id="city" />
-      </FormGroup>
-      <FormGroup>
-        <Label >Zip Code</Label>
-        <Input type="text" name="text" id="zipCode" />
-      </FormGroup>
-      <FormGroup>
-        <Label >Select Wifi session</Label>
-        <Input type="select" name="select" id="Type">
-          <option>Free</option>
-          <option>Limit Free</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Location-Type</Label>
-        <Input type="textarea" name="text" id="locationType" />
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
+      <Form className='hotspot-form' onSubmit={onFormSubmit}>
+        <h1 className='text-center'>Add New Hotspot</h1>
+        <FormGroup>
+          <Label >Hotspot Name</Label>
+          <Input type="text" name="text" id="hotspotName"/>
+        </FormGroup>
+        <FormGroup>
+          <Label >Address</Label>
+          <Input type="text" name="text" id="location" />
+        </FormGroup>
+        <FormGroup>
+          <Label >City</Label>
+          <Input type="text" name="text" id="city" />
+        </FormGroup>
+        <FormGroup>
+          <Label >Zip Code</Label>
+          <Input type="text" name="text" id="zipCode" />
+        </FormGroup>
+        <FormGroup>
+          <Label >Select Wifi session</Label>
+          <Input type="select" name="select" id="Type">
+            <option>Free</option>
+            <option>Limit Free</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Location-Type</Label>
+          <Input type="textarea" name="text" id="locationType" />
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+      </Form>
       </div>
     )
   }  
